188074bf76ea397a92a57ee71c0060bc7aede67c
Draco1995 <349084298@qq.com>
2017-06-05 11:18:23 +0200

    test
    test

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 20:12:49 2017
+
+@author: MSI
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Hello World")
+
+# main program starts here
+hello()
