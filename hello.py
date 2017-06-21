417e2b8a7390b776432e3f39253ba78d48f906e1
Draco1995 <349084298@qq.com>
2017-06-22 00:36:17 +0200

    今天真TM辛苦
    是的
    Its true
    Cest vrai!

--- hello.py
+++ hello.py
@@ -170,10 +170,90 @@ def estiCov(X):
         for q in range(length):
             b = X[:,q]
             b -= np.mean(b)
-            sigma[p,q] = np.dot(np.transpose(b),a)[0,0]/sample
+            sigma[p,q] = np.dot(np.transpose(b),a)[0,0]/(sample-1)
     return sigma
             
+def question8(X,number):
+    i = 0
+    j = 0
+    ans = []
+    for i in range(len(X[:,0])):
+        for j in range(i,len(X[:,0])):
+            if abs(X[i,j])<number:
+                ans.append([i+1,j+1])
+    return ans
+
+def densite_logistique(x):
+    return (2*np.exp(-2*x))/(1+np.exp(-2*x))/(1+np.exp(-2*x))
+
+
+
+def question10a():
+    x = np.linspace(-3,3,100)
+    E = np.random.rand(1000)
+    E = 0.5*np.log(E/(1-E))
+    plt.hist(E,bins = 80,normed = 1, label = "Stimulation")
+    plt.plot(x,densite_logistique(x), label = "densite de la loi logistique")
+    
+def question10b():
+    A = np.matrix([
+            [1,2,3],
+            [3,5,2],
+            [1,4,7],
+            [2,3,4],
+            [2,4,5],
+            [2,9,2],
+            [13,2,1]
+            ])
+    E = np.random.rand(3)
+    E = 0.5*np.log(E/(1-E))
+    X = np.dot(A,E)       #C'est un echantillon! XD
+    return X
+
+def gamma(W,X1):
+    a = len(W[:,0])
+    b = X1.shape[1]
+    ans = np.eye(a,b)
+    
+    for j in range(a):
+        for m in range(b):
+            ans[j,m]=0
+            for i in range(len(X1[:,0])):
+                ans[j,m]+=(   densite_logistique(   np.dot(W,np.transpose(X1[i,:]))[j]  )     )*X1[i,m]
+            ans/=len(X1[:,0])
+    return ans
         
+
+def gradAscent(W,n,p,X): #W Start n numbers p pace X echantillon
+    for i in range(n):
+        G = gamma(W,X)
+        M = np.dot(G,np.dot(np.transpose(W),W))
+        M2 = W+M
+        W = W + p * M2
+        #print(W)
+    #print(W)
+    return W
+
+
+
+def question17(B):
+   # plt.figure()
+   # plt.scatter(B[:,0],B[:,1])
+    W = np.matrix([
+            [-4,-0.5],
+            [-2,1],
+            ])
+    W = gradAscent(W,500,0.005,B[:,0:2])
+    #print(W)
+    x = np.linspace(-1,5,100)
+    A = np.linalg.inv(W)
+    #print(A)
+   # plt.plot(x,A[0,0]/A[1,0]*x)
+   # plt.plot(x,A[0,1]/A[1,1]*x)
+    plt.figure()
+    plt.scatter(B[:,0],B[:,1])
+    plt.plot(x,A[1,0]/A[0,0]*x)
+    plt.plot(x,A[1,1]/A[0,1]*x)
 # main program starts here
 x = read()
 List_matrix = text_to_matrix(x)
@@ -185,4 +265,18 @@ X = question2(A)
 #question4(X,valeur_propre,vecteur_propre)
 #question5(valeur_propre,vecteur_propre)
 #question6(X,valeur_propre,vecteur_propre)
-print(estiCov(X))
+#print("12132132321312312232")
+#print(question8(estiCov(X),0.1))
+
+
+
+#question10a()
+#print(question10b())
+
+B = question2(B)
+question17(B)
+valeur_prorpre,vecteur_propre = question3(B[:,0:2])
+print(vecteur_propre)
+x = np.linspace(-1,5,100)
+plt.plot(x,vecteur_propre[1,0]/vecteur_propre[0,0]*x)
+plt.plot(x,vecteur_propre[1,1]/vecteur_propre[0,1]*x)
